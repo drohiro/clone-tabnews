c4a48c1239603b26ad14993631a5e1910a0cae44
Rodrigo Ferreira <rodrigorfsfr3@gmail.com>
2026-07-17 18:52:42 +0000

    add gtm for tests

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,40 @@
-function Home() {
-  return <h1>Site em construcao</h1>;
-}
+import Head from "next/head";
+import Script from "next/script";
 
-export default Home;
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Clone TabNews</title>
+
+        <Script id="gtm" strategy="afterInteractive">
+          {`
+          (function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+            var f = d.getElementsByTagName(s)[0],
+              j = d.createElement(s),
+              dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+          })(window, document, 'script', 'dataLayer', 'GTM-N33XNNVG');
+        `}
+        </Script>
+      </Head>
+
+      <main>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N33XNNVG"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        <h1>Site em construcao</h1>
+      </main>
+    </>
+  );
+}
