45c03b0579495f0e15448fbfddb4b4c5c6d762c2
Rodrigo Ferreira <rodrigorfsfr3@gmail.com>
2026-07-17 19:03:24 +0000

    add tag vv for tests

--- pages/index.js
+++ pages/index.js
@@ -6,33 +6,14 @@ export default function Home() {
     <>
       <Head>
         <title>Clone TabNews</title>
-
-        <Script id="gtm" strategy="afterInteractive">
-          {`
-          (function (w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-            var f = d.getElementsByTagName(s)[0],
-              j = d.createElement(s),
-              dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-          })(window, document, 'script', 'dataLayer', 'GTM-N33XNNVG');
-        `}
-        </Script>
+        <script
+          src="//collect.vendavalida.com.br/push.js"
+          async
+          type="text/javascript"
+        ></script>
       </Head>
 
       <main>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-N33XNNVG"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-
         <h1>Site em construcao</h1>
       </main>
     </>
